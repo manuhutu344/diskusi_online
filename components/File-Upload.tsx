13decb92aa06bfa62960f9493025fe97880f0124
manuhutu344 <nandoperdos2@gmail.com>
2024-01-22 20:23:12 +0900

    Membuat tombol ganti foto server

--- components/File-Upload.tsx
+++ components/File-Upload.tsx
@@ -23,6 +23,9 @@ function FileUpload({onChange, value, endpoint}:Props) {
           alt = 'Upload'
           className='rounded-full' 
          />
+         <button onClick={()=>onChange("")} className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm' type='button'>
+            <X className='h-4 w-4' />
+         </button>
       </div>
     )
   }
