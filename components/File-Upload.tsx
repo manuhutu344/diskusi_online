4fbaa591bc5e1f7c6225daab922a3a3dc4aebe0c
manuhutu344 <nandoperdos2@gmail.com>
2024-01-22 20:19:05 +0900

    Membuat gambar server muncul

--- components/File-Upload.tsx
+++ components/File-Upload.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import React from 'react'
 import {UploadDropzone} from '@/lib/uploading'
 import '@uploadthing/react/styles.css'
+import {X} from "lucide-react"
+import Image from 'next/image'
 
 interface Props{
     onChange: (url?: string) => void
@@ -9,11 +13,24 @@ interface Props{
 }
 
 function FileUpload({onChange, value, endpoint}:Props) {
+  const fileType = value?.split(".").pop()
+  if(value && fileType !=="pdf"){
+    return(
+      <div className='relative h-20 w-20'>
+        <Image
+          fill
+          src={value}
+          alt = 'Upload'
+          className='rounded-full' 
+         />
+      </div>
+    )
+  }
   return (
     <UploadDropzone
     endpoint={endpoint}
     onClientUploadComplete={(res)=>{
-        onChange(res?.[0].fileUrl)
+        onChange(res?.[0].url)
     }}
     onUploadError={(error: Error)=>{
         console.log(error)
