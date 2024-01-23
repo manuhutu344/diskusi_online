47bd4d4ba2268cefbe2fc44b2c7446e882c1469a
manuhutu344 <nandoperdos2@gmail.com>
2024-01-23 18:06:41 +0900

    Mempercantik plus tombol

--- components/NavigationAction.tsx
+++ components/NavigationAction.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 function NavigationAction() {
   return (
     <div>
-        <button className='group'>
+        <button className='group flex items-center'>
             <div className='flex 
             mx-3 h-[48px] w-[48px] 
             rounded-[24px] group-hover:rounded-[16px] 
