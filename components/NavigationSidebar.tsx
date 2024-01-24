ab771e369887d5ca377857530d6aaecefb692faf
manuhutu344 <nandoperdos2@gmail.com>
2024-01-25 08:00:11 +0900

    Memunculkan nama server menggunakan scrollarea

--- components/NavigationSidebar.tsx
+++ components/NavigationSidebar.tsx
@@ -3,6 +3,8 @@ import { db } from '@/lib/db'
 import { redirect } from 'next/navigation'
 import React from 'react'
 import NavigationAction from './NavigationAction'
+import { Separator } from './ui/separator'
+import { ScrollArea } from './ui/scroll-area'
 
 async function NavigationSidebar() {
     const profile = await currentProfile()
@@ -24,6 +26,16 @@ async function NavigationSidebar() {
   return (
     <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
         <NavigationAction />
+        <Separator 
+            className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto'
+         />
+         <ScrollArea className='flex-1 w-full'>
+            {(await server).map((server)=>(
+                <div key={server.id}>
+                    {server.name}
+                </div>
+            ))}
+         </ScrollArea>
     </div>
   )
 }
