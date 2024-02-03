e3144e7c79811751e39045d53cadc79547aa3520
manuhutu344 <nandoperdos2@gmail.com>
2024-02-03 10:31:32 +0900

    Membuat untuk dapat generet link baru

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -8,10 +8,11 @@ import { Button } from '../ui/button'
 import { Check, Copy, RefreshCw } from 'lucide-react'
 import { useOrigin } from '@/hooks/use-origin'
 import { useState } from 'react'
+import axios from 'axios'
 
 
 export function InviteModal(){
-    const {isOpen, onClose, type, data} = useModal()
+    const {isOpen, onClose, type, data, onOpen} = useModal()
     const origin = useOrigin()
     const isModalOpen = isOpen && type === "Undang"
     const {server} = data
@@ -27,6 +28,18 @@ export function InviteModal(){
         }, 1000)
     }
 
+    async function onNew(){
+        try {
+            setIsLoading(true)
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`)
+            onOpen("Undang", {server: response.data})
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
     return(
         <Dialog open={isModalOpen} onOpenChange={onClose}>
             <DialogContent className='bg-white text-black p-0 overflow-hidden'>
@@ -40,16 +53,16 @@ export function InviteModal(){
                         Invite Melalui Link
                     </Label>
                     <div className='flex items-center mt-2 gap-x-2'>
-                        <Input className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus:ring-offset-0'
+                        <Input disabled={isLoading} className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus:ring-offset-0'
                             value={inviteUrl}
                          />
-                         <Button onClick={onCopy} size='icon'>
+                         <Button disabled={isLoading} onClick={onCopy} size='icon'>
                             {
                                 copied ? <Check className='w-4 h-4' /> : <Copy className='w-4 h-4' />
                             }
                          </Button>
                     </div>
-                    <Button variant='link' size='sm' className='text-xs text-zinc-500 mt-4'>
+                    <Button onClick={onNew} disabled={isLoading} variant='link' size='sm' className='text-xs text-zinc-500 mt-4'>
                         Generate Link Nya 
                         <RefreshCw className='w-4 h-4 ml-2' />
                     </Button>
