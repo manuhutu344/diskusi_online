134bfe393a739c233185cb1d812615f8413bc580
manuhutu344 <nandoperdos2@gmail.com>
2024-02-27 19:33:00 +0900

    Membuat api untuk edit channel

--- hooks/use-model-store.ts
+++ hooks/use-model-store.ts
@@ -1,7 +1,7 @@
 import { Server, ChanelType, Chanel } from '@prisma/client'
 import {create} from 'zustand'
 
-export type ModalType = "Membuat Server" | "Undang" | "Edit Server" | 'member'| 'Buat Channel' | 'Tinggalkan Server' | 'Hapus Server' | 'Hapus Channel'
+export type ModalType = "Membuat Server" | "Undang" | "Edit Server" | 'member'| 'Buat Channel' | 'Tinggalkan Server' | 'Hapus Server' | 'Hapus Channel' | 'Edit Channel'
 
 interface ModalData{
     server?: Server
