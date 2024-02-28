ac1f1d165b9b76b5b1a22a09f545fec0ccb0ab5c
manuhutu344 <nandoperdos2@gmail.com>
2024-02-28 18:41:50 +0900

    Membuat agar di pencet channel ke chatnya

--- components/ServerChannel.tsx
+++ components/ServerChannel.tsx
@@ -6,7 +6,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from 'lucide-react'
 import { useParams, useRouter } from 'next/navigation'
 import React from 'react'
 import ActionTooltip from './action-tooltip'
-import { useModal } from '@/hooks/use-model-store'
+import { ModalType, useModal } from '@/hooks/use-model-store'
 
 interface Props{
     channel: Chanel
@@ -25,8 +25,18 @@ function ServerChannel({channel, server, role}:Props) {
     const router = useRouter()
     const Icon = iconMap[channel.type]
     const {onOpen} = useModal()
+
+    function onClick(){
+        router.push(`/servers/${params?.serverId}/channels/${channel.id}`)
+    }
+
+    function onAction(e:React.MouseEvent, action: ModalType){
+        e.stopPropagation()
+        onOpen(action, {channel, server})
+    }
+
   return (
-    <button onClick={()=>{}} className={cn(
+    <button onClick={onClick} className={cn(
         'group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
     )}>
@@ -40,10 +50,10 @@ function ServerChannel({channel, server, role}:Props) {
         {channel.name !== "general" && role !== MemberRole.GUEST && (
             <div className='ml-auto flex items-center gap-x-2'>
                 <ActionTooltip label='Edit'>
-                    <Edit onClick={()=>onOpen('Edit Channel', {server, channel})} className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition' />
+                    <Edit onClick={(e)=>onAction(e, 'Edit Channel')} className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition' />
                 </ActionTooltip>
                 <ActionTooltip label='Hapus'>
-                    <Trash onClick={()=>onOpen("Hapus Channel", {server, channel})} className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition' />
+                    <Trash onClick={(e)=>onAction(e, "Hapus Channel")} className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition' />
                 </ActionTooltip>
             </div>
         )} 
