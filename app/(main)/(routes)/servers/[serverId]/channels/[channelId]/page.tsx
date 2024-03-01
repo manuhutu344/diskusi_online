3d6c86472ac54dcdb12f85db686a577c8fe4d291
manuhutu344 <nandoperdos2@gmail.com>
2024-03-01 20:24:32 +0900

    Membuat header di bagian chatt

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,45 @@
+import ChatHeader from '@/components/chat/ChatHeader'
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-function page() {
+interface Props{
+  params:{
+    serverId: string
+    channelId: string
+  }
+}
+
+async function page({params}:Props) {
+  const profile = await currentProfile()
+
+  if(!profile){
+    return redirectToSignIn()
+  }
+
+  const channel = await db.chanel.findUnique({
+    where:{
+      id: params.channelId
+    }
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    }
+  })
+
+  if(!channel || !member){
+    redirect("/")
+  }
+
   return (
-    <div>page ini bos</div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
+    </div>
   )
 }
 
