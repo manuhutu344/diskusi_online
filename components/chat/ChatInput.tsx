5e434f3d29fd09631780f4e266ac874f4fffec80
manuhutu344 <nandoperdos2@gmail.com>
2024-03-20 19:55:46 +0900

    Membuat dapat mengirim file pada saat chat

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -8,6 +8,7 @@ import {Input} from '@/components/ui/input'
 import { Plus, Smile } from 'lucide-react'
 import axios from 'axios'
 import qs from 'query-string'
+import { useModal } from '@/hooks/use-model-store'
 
 interface Props{
     apiUrl: string
@@ -21,6 +22,7 @@ const formSchema = z.object({
 })
 
 function ChatInput({apiUrl, query, name, type}:Props) {
+    const {onOpen} = useModal()
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -46,7 +48,7 @@ function ChatInput({apiUrl, query, name, type}:Props) {
                 <FormItem>
                     <FormControl>
                         <div className="relative p-4 pb-6">
-                            <button type="button" onClick={()=>{}} className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center">
+                            <button type="button" onClick={()=>onOpen('File', {apiUrl, query})} className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center">
                                 <Plus className="text-white dark:text-[#313338]" />
                             </button>
                             <Input disabled={isLoading} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible: ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" placeholder={`Chat ${type === "conversation" ? name : "#" + name}`} {...field} />
--- hooks/use-model-store.ts
+++ hooks/use-model-store.ts
@@ -1,12 +1,14 @@
 import { Server, ChanelType, Chanel } from '@prisma/client'
 import {create} from 'zustand'
 
-export type ModalType = "Membuat Server" | "Undang" | "Edit Server" | 'member'| 'Buat Channel' | 'Tinggalkan Server' | 'Hapus Server' | 'Hapus Channel' | 'Edit Channel'
+export type ModalType = "Membuat Server" | "Undang" | "Edit Server" | 'member'| 'Buat Channel' | 'Tinggalkan Server' | 'Hapus Server' | 'Hapus Channel' | 'Edit Channel' | 'File'
 
 interface ModalData{
     server?: Server
     channel?: Chanel
     channelType?: ChanelType
+    apiUrl?: string
+    query?: Record<string, any>
 }
 
 interface Props{
