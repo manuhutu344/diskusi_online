f904b0d6634b5779f5a8a43ae0bc75ece82957ac
manuhutu344 <nandoperdos2@gmail.com>
2024-03-22 19:12:15 +0900

    Membuat icon dan rfresh chat saat selesai kirim

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -5,10 +5,12 @@ import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {Form, FormControl, FormField, FormItem} from '@/components/ui/form'
 import {Input} from '@/components/ui/input'
-import { Plus, Smile, Video } from 'lucide-react'
+import { Plus } from 'lucide-react'
 import axios from 'axios'
 import qs from 'query-string'
 import { useModal } from '@/hooks/use-model-store'
+import EmojiPicker from '../EmojiPicker'
+import { useRouter } from 'next/navigation'
 
 interface Props{
     apiUrl: string
@@ -23,6 +25,7 @@ const formSchema = z.object({
 
 function ChatInput({apiUrl, query, name, type}:Props) {
     const {onOpen} = useModal()
+    const router = useRouter()
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -37,6 +40,8 @@ function ChatInput({apiUrl, query, name, type}:Props) {
                 query
             })
             await axios.post(url, values)
+            form.reset()
+            router.refresh()
         } catch (error) {
             console.log(error)
         }
@@ -55,7 +60,7 @@ function ChatInput({apiUrl, query, name, type}:Props) {
                             </div>
                             <Input disabled={isLoading} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible: ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" placeholder={`Chat ${type === "conversation" ? name : "#" + name}`} {...field} />
                             <div className="absolute top-7 right-8">
-                                <Smile  />
+                                <EmojiPicker onChange={(emoji: string)=>field.onChange(`${field.value} ${emoji}`)} />
                             </div>
                         </div>
                     </FormControl>
